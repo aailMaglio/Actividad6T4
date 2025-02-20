4f54119b72394af9e03f097c372ddf793677e0e1
Carlos <darkphoenix67cdlc@hotmail.com>
2025-02-20 19:41:43 +0000

    Método ComprobarDni

--- LetraDni/src/main/java/com/lorenzo/letradni/LetraDni.java
+++ LetraDni/src/main/java/com/lorenzo/letradni/LetraDni.java
@@ -18,6 +18,14 @@ public class LetraDni {
         String letras = "TRWAGMYFPDXBNJZSQVHLCKE";
         return letras.charAt(dni % 23);
     }
+    public static boolean ComprobarDNI(String dniCompleto) {
+    if (dniCompleto.length() != 9) return false;
+
+    int numero = Integer.parseInt(dniCompleto.substring(0, 8));
+    char letra = dniCompleto.charAt(8);
+
+    return AdivinarNumeroDNI(numero) == letra;
+}
     
     public static void main(String[] args) {
         int numeroDNI = 12345678;
