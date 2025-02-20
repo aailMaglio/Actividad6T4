0723c94b198df51989957e0d7764957838ca2f8a
Carlos <darkphoenix67cdlc@hotmail.com>
2025-02-20 19:37:59 +0000

    Método LertraDni

--- LetraDni/src/main/java/com/lorenzo/letradni/LetraDni.java
+++ LetraDni/src/main/java/com/lorenzo/letradni/LetraDni.java
@@ -13,4 +13,15 @@ public class LetraDni {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+    public class LetraDNI {
+    public static char AdivinarNumeroDNI(int dni) {
+        String letras = "TRWAGMYFPDXBNJZSQVHLCKE";
+        return letras.charAt(dni % 23);
+    }
+    
+    public static void main(String[] args) {
+        int numeroDNI = 12345678;
+        System.out.println("La letra del DNI es: " + AdivinarNumeroDNI(numeroDNI));
+    }
+}
 }
