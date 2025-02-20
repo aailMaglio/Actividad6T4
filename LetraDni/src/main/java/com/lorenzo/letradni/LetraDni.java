90886dce96d1def99a9e6a34795a8767dc4d4e8f
Lorenzo <lorenzo.maglio041@gmail.com>
2025-02-20 17:48:24 +0000

    PROYECTO INICIAL

---
+++ LetraDni/src/main/java/com/lorenzo/letradni/LetraDni.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.lorenzo.letradni;
+
+/**
+ *
+ * @author lollo
+ */
+public class LetraDni {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
